534c9a070a83fe88e222393859ba03371fcbab98
dimitriantoniou <dma2012@gmail.com>
2020-08-07 18:20:55 -0500

    working on coffee project

--- main.js
+++ main.js
@@ -1,65 +1,5 @@
 "use strict"
 
-/* oriignal function
-function renderCoffee(coffee) {
-    var html = '<tr class="coffee">';
-    html += '<td>' + coffee.id + '</td>';
-    html += '<td>' + coffee.name + '</td>';
-    html += '<td>' + coffee.roast + '</td>';
-    html += '</tr>';
-
-    return html;
-}
- */
-function renderCoffee(coffee) {
-    var html = '<h1 class="coffee">';
-    html += '<h1>' + coffee.id + '</h1>';
-    html += '<h1>' + coffee.name + '</h1>';
-    html += '<h1>' + coffee.roast + '</h1>';
-    html += '</h1>'
-}
-/*
-creates a function renderCoffee that takes in a variable coffee
-defines a variable html equal to a table row with class coffee
-adds table cells that take the object coffee and assign properties id, name, and roast
-    */
-
-
-function renderCoffees(coffees) {
-    var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
-        html += renderCoffee(coffees[i]);
-    }
-    return html;
-}
-
-/*
-defines a function renderCoffees (plural) that takes in the object coffees (plural)
-creates a for loop beginning at i that will run as long as the length of coffee-1; after running, decrements by 1
-sets html = the output of the function renderCoffee after passing the index i of the object coffees
-*/
-
-function updateCoffees(e) {
-    e.preventDefault(); // don't submit the form, we just want to update the data
-    var selectedRoast = roastSelection.value;
-    var filteredCoffees = [];
-    coffees.forEach(function(coffee) {
-        if (coffee.roast === selectedRoast) {
-            filteredCoffees.push(coffee);
-        }
-    });
-    tbody.innerHTML = renderCoffees(filteredCoffees);
-}
-/*
-defines a function updateCoffees that takes in object e
-declares a variable selectedRoast equal to the property value of the object roastSelection
-declares a variable filteredCoffees that is equal to an empty array
-creates a for each loop
-runs a foreach loop on the array coffees, executing the function on the  coffee element
-if the roast attribute of the coffee element is the same type and valye as the selectedRoast
-then add the coffee to the filteredCoffees array
- */
-
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
     {id: 1, name: 'Light City', roast: 'light'},
@@ -82,6 +22,28 @@ var tbody = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
 var roastSelection = document.querySelector('#roast-selection');
 
-tbody.innerHTML = renderCoffees(coffees);
 
-submitButton.addEventListener('click', updateCoffees);
+function renderCoffee(coffee) {
+    var html = '<div>';
+    // html += '' + coffee.id + '';
+    html += '<h2>' + coffee.name + '</h2>';
+    html += '<p>' + coffee.roast + '</p>';
+    html += '</div>';
+    return html;
+}
+
+function renderCoffees(coffees) {
+    var html = '';
+    for(var i = coffees.length -1; i>=0; i--) {
+        html += renderCoffee(coffees[i]);
+    }
+    return html;
+}
+
+    var tbody = document.querySelector('#coffees');
+    var search = document.querySelector("#coffee");
+    var submitButton = document.querySelector('#submit');
+    var roastSelection = document.querySelector('#roast-selection');
+
+console.log(coffees);
+
