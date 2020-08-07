68e924defa5d1233892859f49b1d5a612ee3aaa1
dimitriantoniou <dma2012@gmail.com>
2020-08-07 16:24:54 -0500

    coffee project first function

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 "use strict"
 
+/* oriignal function
 function renderCoffee(coffee) {
     var html = '<tr class="coffee">';
     html += '<td>' + coffee.id + '</td>';
@@ -9,6 +10,20 @@ function renderCoffee(coffee) {
 
     return html;
 }
+ */
+function renderCoffee(coffee) {
+    var html = '<h1 class="coffee">';
+    html += '<h1>' + coffee.id + '</h1>';
+    html += '<h1>' + coffee.name + '</h1>';
+    html += '<h1>' + coffee.roast + '</h1>';
+    html += '</h1>'
+}
+/*
+creates a function renderCoffee that takes in a variable coffee
+defines a variable html equal to a table row with class coffee
+adds table cells that take the object coffee and assign properties id, name, and roast
+    */
+
 
 function renderCoffees(coffees) {
     var html = '';
@@ -17,6 +32,12 @@ function renderCoffees(coffees) {
     }
     return html;
 }
+
+/*
+defines a function renderCoffees (plural) that takes in the object coffees (plural)
+creates a for loop beginning at i that will run as long as the length of coffee-1; after running, decrements by 1
+sets html = the output of the function renderCoffee after passing the index i of the object coffees
+*/
 
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
@@ -29,6 +50,15 @@ function updateCoffees(e) {
     });
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
+/*
+defines a function updateCoffees that takes in object e
+declares a variable selectedRoast equal to the property value of the object roastSelection
+declares a variable filteredCoffees that is equal to an empty array
+creates a for each loop
+runs a foreach loop on the array coffees, executing the function on the  coffee element
+if the roast attribute of the coffee element is the same type and valye as the selectedRoast
+then add the coffee to the filteredCoffees array
+ */
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
